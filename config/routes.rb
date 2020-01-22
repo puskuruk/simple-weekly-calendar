1611aab884f5ea1be8bb427dea4d098bc867af5b
Tamay Eser Uysal <tamayeseruysal@gmail.com>
2020-01-22 08:05:24 +0300

    Created /pages for serving vuejs application and homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'pages/index'
+  get 'pages/calendar'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
