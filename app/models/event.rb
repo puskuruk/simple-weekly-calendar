926a600d9219b6266ab63edcb8a7058c5075653c
Tamay Eser Uysal <tamayeseruysal@gmail.com>
2020-01-23 23:09:17 +0300

    Added creating color for every record and finding current event functions to model

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,13 +3,27 @@ class Event < ApplicationRecord
 
     after_create :add_color_to_event
 
+
+
     def add_color_to_event
         random_color = generate_rgb()
         self.update(:color => random_color )
     end
 
-    private
+    def self.events_in_week(current_date)
+        current_date = Date.parse(current_date) rescue nil
+        if current_date != nil
+            events = Array.new
+            sunday = current_date.days_ago(current_date.wday)
 
+            events = Event.where(start: current_date.beginning_of_week..current_date.end_of_week)
+        else
+            events = nil
+        end
+    end
+
+    private
+    
     def generate_rgb
         color_array = Array.new
         3.times do
